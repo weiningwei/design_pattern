fc8514663171b1ae125ded17a92c34e83e849fbe
weiningwei <1479796521@qq.com>
2024-03-22 20:28:35 +0800

    future(简单工厂模式):添加简单工程模式,实现加减乘除计算器

--- src/01_simple_factory/operation.cpp
+++ src/01_simple_factory/operation.cpp
@@ -1,8 +1,93 @@
-#include "operation.h"
 #include <iostream>
+#include <string>
+#include "operation.h"
+
+class OperateAdd : public Operation
+{
+public:
+    double getResult() const override
+    {
+        double result = 0;
+        result = m_dNumberA + m_dNumberB;
+        return result;
+    }
+};
+
+class OperateSub : public Operation
+{
+public:
+    double getResult() const override
+    {
+        double result = 0;
+        result = m_dNumberA - m_dNumberB;
+        return result;
+    }
+};
+
+class OperateMul : public Operation
+{
+public:
+    double getResult() const override
+    {
+        double result = 0;
+        result = m_dNumberA * m_dNumberB;
+        return result;
+    }
+};
+
+class OperateDiv : public Operation
+{
+public:
+    double getResult() const override
+    {
+        double result = 0;
+        result = m_dNumberA / m_dNumberB;
+        return result;
+    }
+};
+
+Operation *OperationFactory::createOperate(const char operate)
+{
+    Operation *oper;
+    switch (operate)
+    {
+    case '+':
+        oper = new OperateAdd();
+        break;
+    case '-':
+        oper = new OperateSub();
+        break;
+    case '*':
+        oper = new OperateMul();
+        break;
+    case '/':
+        oper = new OperateDiv();
+        break;
+
+    default:
+        break;
+    }
+    return oper;
+}
 
 int main(void)
 {
-    std::cout << "hello world\n";
+    // std::string opertate;
+    // std::cin >> opertate;
+    std::string opertate = "+-*/";
+    double numA = 4;
+    double numB = 2;
+    Operation *oper;
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        oper = OperationFactory::createOperate(opertate[i]);
+        oper->setNumberA(numA);
+        oper->setNumberB(numB);
+        double result = oper->getResult();
+
+        std::cout << numA << " " << opertate[i] << " " << numB << " = " << result << "\n";
+    }
+
     return 0;
 }
--- src/01_simple_factory/operation.h
+++ src/01_simple_factory/operation.h
@@ -1,5 +1,31 @@
-#ifdef OPERATION_H
+#ifndef OPERATION_H
 #define OPERATION_H
 
+class Operation
+{
+private:
+    /* data */
+public:
+
+    virtual double getResult() const = 0;
+    void setNumberA(double numberA)
+    {
+        m_dNumberA = numberA;
+    }
+    void setNumberB(double numberB)
+    {
+        m_dNumberB = numberB;
+    }
+
+protected:
+    double m_dNumberA;
+    double m_dNumberB;
+};
+
+class OperationFactory
+{
+public:
+    static Operation *createOperate(const char operate);
+};
 
 #endif
